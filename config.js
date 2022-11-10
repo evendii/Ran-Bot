309208dd14d71f7ff33d500a760d182aeaa8748d
evendii <107420960+evendii@users.noreply.github.com>
2022-11-10 13:14:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -39,7 +39,7 @@ function kyun(seconds){
 }
 					runtime = process.uptime()
 					teks = `${kyun(runtime)}`
-global.owner = ['17608914335', '0' , '436506665652696', '6283820073017', '6289637133848'] // Put your number here
+global.owner = ['6289522183114', '0' , '6289522183114','289522183114'] // Put your number here
 global.fake = ['17608914335', '0', '0', '0']
 global.fakeNum = ['17608914335','0' ]
 global.mods = [] // Want some help?
@@ -68,7 +68,7 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botwm = `©Ran_2022`//UBAH JADI NAMAMU
+global.botwm = `©MEA_2022`//UBAH JADI NAMAMU
 global.botdate = `╭──────────═┅═─────────\n│◪ ${time}\n┊◪ ${week} ${date}\n╰──────────═┅═─────────`
 global.packname =  `Ran`
 global.author = `Mitake`
